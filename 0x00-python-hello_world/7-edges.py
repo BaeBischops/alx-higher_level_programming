8964a3c9069fe8feab3bb486872e680af7d8e85a
BaeBischops <dr.bkzee@gmail.com>
2023-12-04 02:05:53 -0800

    the update of the 7-edges.py file

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,6 +4,6 @@ word_first_3, *middle_word, word_last_2 = word
 word_first_3 = ''.join(word_first_3)
 word_last_2 = ''.join(word_last_2)
 middle_word = ''.join(middle_word)
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
+print(f"First three letters: {word_first_3}")
+print(f"Last two letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
