181906c6052ad74eb2557d608f9b4300e8e141ce
BaeBischops <dr.bkzee@gmail.com>
2023-12-23 00:27:27 -0800

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,56 +1,82 @@
 #!/usr/bin/python3
+"""module for a singly linked list"""
+
+
 class Node:
+    """"defines a node"""
+
     def __init__(self, data, next_node=None):
+        """initializes the node with instance variables"""
+
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        return self.__data
+        """gets data attribute"""
+
+        return (self.__data)
 
     @data.setter
     def data(self, value):
+        """sets data attribute"""
+
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-        return self.__next_node
+        """get next_node attribute
+        Returns: next node
+        """
+
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None and type(value) != Node:
+        """set value of next node"""
+
+        if (value is not None and not isinstance(value, Node)):
             raise TypeError('next_node must be a Node object')
+
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """defines a singly linked list"""
+
     def __init__(self):
+        """Initializes the singly linked list"""
+
         self.head = None
 
     def __str__(self):
-        ret = ""
-        node = self.head
-        while node:
-            ret += str(node.data) + "\n"
-            node = node.next_node
-        return ret[:-1]
+        """make list printable"""
+
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
 
     def sorted_insert(self, value):
+        """insert in a sorted fashion
+        Args:
+            value: what the value will be on the node
+        """
         new = Node(value)
         if not self.head:
             self.head = new
             return
-
         if value < self.head.data:
             new.next_node = self.head
             self.head = new
             return
-
-        node = self.head
-        while node.next_node and node.next_node.data < value:
-            node = node.next_node
-        if node.next_node:
-            new.next_node = node.next_node
-        node.next_node = new
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
