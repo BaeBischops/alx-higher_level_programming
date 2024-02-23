dba7d169cba562a075e29c1d6be984c0248f8389
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2024-02-23 21:13:43 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""
+contains the MyList class
+"""
 
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    def print_sorted(self):
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
