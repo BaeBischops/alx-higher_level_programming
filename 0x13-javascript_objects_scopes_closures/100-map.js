5e065d68508793296212b4355a3e60fa8f5d2c35
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2024-07-15 11:58:58 +0200

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
 console.log(list);
-console.log(list.map((num, i) => num * i));
+console.log(newList);
