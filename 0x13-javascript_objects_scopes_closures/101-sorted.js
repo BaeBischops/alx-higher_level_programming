3e809ad45a460efcc630c6d468806467f99e2099
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2024-07-15 11:54:40 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
-const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
-  }
-  newDict[valsUniq[j]] = list;
-}
-console.log(newDict);
+const ids = [...new Set(Object.values(dict))];
+const result = ids.reduce((newDict, id) => {
+  newDict[id] = Object.keys(dict).filter((key) => dict[key] === id);
+  return newDict;
+}, {});
+console.log(result);
